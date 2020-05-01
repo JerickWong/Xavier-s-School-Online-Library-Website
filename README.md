c11bd37103314cf22128fdd195e9dca906056085
JerickWong <42172510+JerickWong@users.noreply.github.com>
2020-05-02 03:45:54 +0800

    initial book class

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 Developed using Python and Django Framework 
 
 To run:
-1. run 'pipenv shell' on terminal 
+1. create a virtual env on terminal 'pipenv shell' 
+2. install django on the created venv 'pipenv install django'
+3. run the server at localhost 3000 'python manage.py runserver 3000'
--- xavier/online_library/models.py
+++ xavier/online_library/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+
+    # Many to Many Field because specs mentioned books can have more than 1 author
+    author = models.ManyToManyField('Author', on_delete=models.SET_NULL, null=True)
+
+    publisher = models.ForeignKey('Publisher', on_delete=models.SET_NULL, null=True)
+    
+    year_of_publication = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
+
+    ISBN = models.CharField('ISBN', max_length=13, help_text='13 Character ISBN number', null=True)
+    status = models.BooleanField(
+        max_length=1,
+        choices=[
+        ('A', 'Available'),
+        ('R', 'Reserved')],
+        default='A',
+        help_text='Book Status'
+    )
+    reviews = models.ManyToManyField(max_length=1000, help_text='Give a review of the book', on_delete=models.SET_NULL, null=True)
