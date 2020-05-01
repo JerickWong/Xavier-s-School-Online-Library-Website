9b3c2c183eb79645bf9b734f743bee3ef8677544
JerickWong <42172510+JerickWong@users.noreply.github.com>
2020-05-02 04:34:31 +0800

    author class

--- xavier/online_library/models.py
+++ xavier/online_library/models.py
@@ -29,4 +29,14 @@ class Book(models.Model):
     def __str__(self):
         return self.title
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField('Died', null=True, blank=True)
 
+    class Meta:
+        ordering = ['last_name', 'first_name']
+
+    def __str__(self):
+        return f'{self.last_name}.[self.first_name]'
