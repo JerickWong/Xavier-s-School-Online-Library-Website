ee168dd5cd7ec4e53ff7d4a80bd52d7457df032e
JerickWong <42172510+JerickWong@users.noreply.github.com>
2020-05-03 18:34:50 +0800

    fixed boolean field and listtextfield issue

--- xavier/online_library/models.py
+++ xavier/online_library/models.py
@@ -15,18 +15,18 @@ class Author(models.Model):
     def __str__(self):
         return f'{self.last_name}.[self.first_name]'
 
-class Book(models.Model, ListTextField):
+class Book(models.Model):
     title = models.CharField(max_length=200)
 
     # Many to Many Field because specs mentioned books can have more than 1 author
-    author = models.ManyToManyField('Author')
+    author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
 
     publisher = models.ForeignKey('Publisher', on_delete=models.SET_NULL, null=True)
     
     year_of_publication = models.DateField(auto_now=False, auto_now_add=False, null=True, blank=True)
 
     ISBN = models.CharField('ISBN', max_length=13, help_text='13 Character ISBN number', null=True)
-    status = models.BooleanField(
+    status = models.CharField(
         max_length=1,
         choices=[
         ('A', 'Available'),
@@ -36,10 +36,17 @@ class Book(models.Model, ListTextField):
     )
 
     # ListTextField is from mysql models. If theres a lot of complications, use ArrayField from PostgreSQL instead
-    reviews = ListTextField(base_field=models.CharField(max_length=200), size=10, help_text='Give a review of the book')
+    
 
     def __str__(self):
         return self.title
+
+class Review(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    review_text = models.TextField(help_text='Write a review for this book')
+
+    def __str__(self):
+        return self.review_text
 
 import uuid # Required for unique book instances
 
--- xavier/xavier/settings.py
+++ xavier/xavier/settings.py
@@ -74,14 +74,21 @@ WSGI_APPLICATION = 'xavier.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'XavierDb',
+#         'USER': 'root',
+#         'PASSWORD': 'password',
+#         'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
+#         'PORT': '3306',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'XavierDb',
-        'USER': 'root',
-        'PASSWORD': 'password',
-        'HOST': '127.0.0.1',   # Or an IP Address that your DB is hosted on
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),        
     }
 }
 
